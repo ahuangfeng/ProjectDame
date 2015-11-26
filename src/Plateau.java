c23ce762aaf953bdf3c28835ab79b58dc3905a62
ahuangfeng <alexhuang05@gmail.com>
2015-11-26 18:54:21 +0100

    Rien

--- src/Plateau.java
+++ src/Plateau.java
@@ -18,8 +18,11 @@ public class Plateau {
 		
 		for(int i=0;i<10;i++){
 			for(int j=0;j<10;j++){
-				//if(i%2)
-				this.tableau[i][j]=new Case(i,j,false);
+				if((i % 2)==0){
+                                    this.tableau[i][j]=new Case(i,j,false,0);
+                                }else{
+                                    this.tableau[i][j]=new Case(i,j,false,1);
+                                }
 			}
 			
 		}
