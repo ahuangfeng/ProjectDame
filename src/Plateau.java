ea593bfbb3fe96c71e89a0e36e9dc0088ad2e359
ahuangfeng <alexhuang05@gmail.com>
2015-12-07 09:44:23 +0100

    new Plateau

--- src/Plateau.java
+++ src/Plateau.java
@@ -1,4 +1,8 @@
 
+import java.awt.*;
+import javax.swing.*;
+
+
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -10,47 +14,32 @@
  *
  * @author AlexHF
  */
-public class Plateau {
+
+public class Plateau extends JPanel{
 	
-	private Case[][] tableau;
 	
 	
-	public Plateau(){
-		this.tableau = new Case[10][10];
-		
-		for(int i=0;i<10;i++){
-			for(int j=0;j<10;j++){
-				
-                            if((i%2)==0){
-                                if(((j%2))==0){
-                                    this.tableau[i][j]=new Case(i,j,false,0);
-                                }else{
-                                    this.tableau[i][j]=new Case(i,j,false,1);
-                                }
-                            }else if((i%2)==1){
-                                if(((j%2))==0){
-                                    this.tableau[i][j]=new Case(i,j,false,1);
-                                }else{
-                                    this.tableau[i][j]=new Case(i,j,false,0);
-                                }
-                            }
-                            
-                            
+	public Plateau() {
+		setLayout(new GridLayout(10,10));
+		for(int i=0; i<10; i++){
+			for(int j=0; j<10; j++){
+				if((j%2==0 && i%2==0) || (j%2!=0 && i%2!=0)){
+					ajouterCase(0);
+				}
+				else{
+					ajouterCase(1);
+				}
 			}
-			
 		}
 	}
-        
-        public String toString(){
-            String res="";
-            for(int i=0;i<this.tableau.length;i++){
-                for(int j=0;j<this.tableau[0].length;j++){
-                    res=res+this.tableau[i][j];
-                }
-                res+= "\n";
-            }
-            return res;
-        }
+	
+	public void ajouterCase(int o){
+		Case case1 = new Case(o);
+		add(case1);
+	}
+	
+	public static void main (String args[]){
+		Plateau o = new Plateau();
+		
+	}
 }
-
-
