3a0adf4ea844a8ddda4febc62274ceaa59d24dc3
ahuangfeng <alexhuang05@gmail.com>
2015-12-13 23:49:35 +0100

    Modification constructeur

--- src/Pion.java
+++ src/Pion.java
@@ -1,3 +1,10 @@
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import javax.swing.JPanel;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,26 +15,36 @@
  *
  * @author AlexHF
  */
-public class Pion {
+public class Pion extends JPanel{
 	
 	private Joueur joueur;
 	private int positionX;
 	private int positionY;
 	private int couleur;
 	
-	public Pion(Joueur joueur,int x,int y,int couleur) {
-		this.joueur=joueur;
-		this.positionX=x;
-		this.positionY=y;
+	public Pion(int couleur) {
+		//this.joueur=joueur;
+		//this.positionX=x;
+		//this.positionY=y;
 		this.couleur=couleur;
+                if(couleur==0){
+                    setForeground(Color.CYAN);
+                }else if(couleur==1){
+                    setForeground(Color.MAGENTA);
+                }
 	}
 	
+        /*public void paintComponent(Graphics g){
+            Paint paint;
+            Graphics2D f;
+            paint=
+        }*/
 	
 	public Joueur getJoueur(){
 		return this.joueur;
 	}
 	
-	public int getPositionX(){
+	/*public int getPositionX(){
 		return this.positionX;
 	}
 	
@@ -42,20 +59,20 @@ public class Pion {
 	public void setPosition(int x, int y){
 		this.positionX=x;
 		this.positionY=y;
-	}
+	}*/
 	
-	public boolean peutBouger(){
+	/*public boolean peutBouger(){
 		//il faut le completer
 		
-	}
+	}*/
 	
-	public boolean peutManger(){
+	/*public boolean peutManger(){
 		//il faut le completer
 	}
 	
 	public void bouger(int posx,int posy){
 		//je crois qu'il sers a rien
-	}
+	}*/
 	
 	public void reset(){
 		this.joueur=null;
