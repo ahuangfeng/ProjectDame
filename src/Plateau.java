11a6803bc59c034cfd48692655335877f21dfc12
ahuangfeng <alexhuang05@gmail.com>
2015-12-13 23:49:11 +0100

    initialiser() getCase()

--- src/Plateau.java
+++ src/Plateau.java
@@ -2,8 +2,6 @@
 import java.awt.*;
 import javax.swing.*;
 
-
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -20,14 +18,14 @@ public class Plateau extends JPanel{
 	public Plateau() {
 		setLayout(new GridLayout(10,10));
 		for(int i=0; i<10; i++){
-			for(int j=0; j<10; j++){
-				if((j%2==0 && i%2==0) || (j%2!=0 && i%2!=0)){
-					ajouterCase(0);
-				}
-				else{
-					ajouterCase(1);
-				}
-			}
+                    for(int j=0; j<10; j++){
+                        if((j%2==0 && i%2==0) || (j%2!=0 && i%2!=0)){
+                            ajouterCase(0);
+                        }
+                        else{
+                            ajouterCase(1);
+                        }
+                    }
 		}
 	}
 	
@@ -35,5 +33,20 @@ public class Plateau extends JPanel{
 		Case case1 = new Case(o);
 		add(case1);
 	}
+        
+        public Pion creerPion(int couleur){
+            Pion pion = new Pion(couleur);
+            //ajouter Listener!
+            return pion;
+        }
+        
+        public void initialiser(){
+            for(int i=0;i<30;i=i+2){
+                getCase(i).add(creerPion(0));
+            }
+        }
+        public Case getCase(int i){
+            return (Case) getComponent(i);
+        }
 	
 }
