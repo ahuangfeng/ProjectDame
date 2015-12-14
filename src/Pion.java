dbe95e70cd28c5a06c5e0fa36696d280381554b3
ahuangfeng <alexhuang05@gmail.com>
2015-12-14 09:29:32 +0100

    Pion fait

--- src/Pion.java
+++ src/Pion.java
@@ -1,5 +1,6 @@
 
 import java.awt.Color;
+import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Paint;
@@ -27,18 +28,30 @@ public class Pion extends JPanel{
 		//this.positionX=x;
 		//this.positionY=y;
 		this.couleur=couleur;
-                if(couleur==0){
-                    setForeground(Color.CYAN);
-                }else if(couleur==1){
-                    setForeground(Color.MAGENTA);
+                if(this.couleur==0){
+                    setForeground(Color.LIGHT_GRAY);
+                    setBackground(Color.WHITE);
+                }else if(this.couleur==1){
+                    setForeground(new Color(102,51,0));
+                    setBackground(Color.LIGHT_GRAY);
                 }
+                setOpaque(false);
 	}
 	
-        /*public void paintComponent(Graphics g){
+        public void paintComponent(Graphics g){
             Paint paint;
-            Graphics2D f;
-            paint=
-        }*/
+            Graphics2D g2d = (Graphics2D) g;
+            if (g instanceof Graphics2D) {
+                g2d = (Graphics2D) g;
+            }
+            else {
+                System.out.println("Error");
+                return;
+            }
+            paint = new GradientPaint(0,0, getBackground(), getWidth(), getHeight(), getForeground());
+            g2d.setPaint(paint);
+            g.fillOval(5, 5, getWidth()-10, getHeight()-10);
+        }
 	
 	public Joueur getJoueur(){
 		return this.joueur;
