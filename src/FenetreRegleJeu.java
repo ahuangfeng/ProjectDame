942f6aeecc0f7a91e824230f35dc1e2eb4f5a4af
aurelien96 <aurelien.g@neuf.fr>
2016-01-07 16:36:14 +0100

    7-1

--- src/FenetreRegleJeu.java
+++ src/FenetreRegleJeu.java
@@ -17,7 +17,7 @@ public class FenetreRegleJeu extends JFrame{
         setSize(400,400);
 
         monPanelAffichageRegle = new JPanel(new BorderLayout());
-
+        //création du JLabel
         this.regle = new JLabel("<html>Objectif : eliminer tous les pions de son adversaire. <br> Regles du jeu : Le jeu se fait sur un plateau de 100 cases (alterne noir et blanc). Les pions se deplacent en diagonale (avant et arriere) d une seule case, sauf lorsqu un pion elimine un pion adverse. Pour eliminer un pion de l adversaire, il faut se trouver a cote du pion, en diagonale et pouvoir sauter au dessus de lui pour pouvoir lui prendre son pion. <br> Option Dame : si un pion arrive sur la premiere ligne de l adversaire, le pion devient alors une dame. Ce nouveau pion peut alors se deplacer, toujours en diagonale, du nombre de case qu il souhaite. Pour eliminer des pions, il doit en revanche pouvoir aussi sauter au-dessus d eux.</html>");
 
         regle.setHorizontalAlignment(SwingConstants.CENTER);
